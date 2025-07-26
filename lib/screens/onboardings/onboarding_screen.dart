5bcdbf386ba4b10934baace4463008e81223282b
Nabeel Tiwana <nabeeltiwana122@gmail.com>
2025-07-26 16:19:08 +0500

    add smooth indicator

--- lib/screens/onboardings/onboarding_screen.dart
+++ lib/screens/onboardings/onboarding_screen.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:lottie/lottie.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:starter_app/helpers/device_helper.dart';
 import 'package:starter_app/screens/onboardings/widget/onboarding_page.dart';
 import 'package:starter_app/utils/constant/images.dart';
 import 'package:starter_app/utils/constant/texts.dart';
 
-import '../../helpers/device_helper.dart';
 
 class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({super.key});
@@ -14,15 +14,17 @@ class OnboardingScreen extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
+          //pageView
            PageView(
              children: [
                OnBoardingPage(animation: MyImages.onboarding1Animation,title: MyText.onBoardingTitle1,subtitle: MyText.onBoardingSubTitle1,),
                OnBoardingPage(animation: MyImages.onboarding2Animation,title: MyText.onBoardingTitle2,subtitle: MyText.onBoardingSubTitle2,),
                OnBoardingPage(animation: MyImages.onboarding3Animation,title: MyText.onBoardingTitle3,subtitle: MyText.onBoardingSubTitle3,),
-
-
              ],
            ),
+          //smooth page indictor
+          OnBoardingDotNavgation()
+
         ],
       ),
     );
@@ -30,3 +32,21 @@ class OnboardingScreen extends StatelessWidget {
 }
 
 
+
+
+class OnBoardingDotNavgation extends StatelessWidget {
+  const OnBoardingDotNavgation({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+        bottom: DeviceHelper.getBottomNavigationBarHeight()*4,
+        left: DeviceHelper.getScreenWidth(context)/2.3,
+        right: DeviceHelper.getScreenHeight(context)/2.3,
+        child: SmoothPageIndicator(controller: PageController(), count: 3,effect: ExpandingDotsEffect(
+          dotHeight: 6.0,
+        ),));
+  }
+}
